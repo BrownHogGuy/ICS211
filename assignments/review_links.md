c45f98d590e0319413327efa86c8277772f63894
Cody Shitagi <codyts@hawaii.edu>
2024-02-06 14:37:18 -1000

    creation of week5

---
+++ assignments/review_links.md
@@ -0,0 +1,4 @@
+# GITHUB REVIEW LINKS
+
+### Starting at week 3; 1/25/24
+1. https://github.com/ICSatKCC/assignment-1-sp24-silverliang.git
---
+++ week4/InterfaceDemo.java
@@ -0,0 +1,18 @@
+/** Interface Demo
+*
+* @author Cody Shitagi
+* @since 02/01/2024
+*/
+
+public class InterfaceDemo {
+  /** main Method.
+  *
+  * <p>args not in use</p>
+  */
+
+  public static void main(String[] args) {
+    System.out.println("hello tuna");
+    
+
+  } // close main
+} // close class
